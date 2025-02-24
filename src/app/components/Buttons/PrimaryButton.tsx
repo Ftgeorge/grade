1334509240c83a830314643eaccfd99dabe24464
ftgeorge <ftgeorgean@gmail.com>
2025-02-24 16:07:16 +0100

    Why choose us

--- src/app/components/Buttons/PrimaryButton.tsx
+++ src/app/components/Buttons/PrimaryButton.tsx
@@ -8,11 +8,11 @@ export default function PrimaryButton({ placeholder, primary }: PrimaryButtonPro
             type="button"
             className={
                 primary
-                    ? `font-montserrat focus:outline-none text-white bg-primary hover:bg-primary focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2`
-                    : 'font-montserrat text-primary hover:text-white border border-primary hover:bg-primary-20 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
+                    ? `font-montserrat focus:outline-none text-white bg-primary hover:bg-primary focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-8 py-3 mb-2`
+                    : 'font-montserrat text-primary hover:text-white border border-primary hover:bg-primary-20 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-full text-sm px-8 py-3 text-center me-2 mb-2'
             }
         >
-            <h1> {placeholder} </h1>
+            <h1 className="font-montserrat"> {placeholder} </h1>
         </button>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import HomePage from "./pages/home/home";
 import Features from "./pages/features/features";
 import HowItWorks from "./pages/how-it-works/how-it-works";
+import WhyChooseUs from "./pages/why-choose-us/whychooseus";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <HomePage/>
     <Features/>
     <HowItWorks/>
+    <WhyChooseUs/>
     </>
   );
 }
